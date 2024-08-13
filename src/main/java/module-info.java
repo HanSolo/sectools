9c2f42643fbb03d3661624166edbb589eba3b5e2
Gerrit Grunwald <han.solo@mac.com>
2024-08-13 12:49:12 +0200

    Added module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module eu.hansolo.sectools {
+    requires com.google.gson;
+    requires java.net.http;
+
+    exports eu.hansolo.sectools;
+    exports eu.hansolo.sectools.metrics;
+    exports eu.hansolo.sectools.severity;
+    exports eu.hansolo.sectools.tools;
+}
